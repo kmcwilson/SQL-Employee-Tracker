9ff2abd6fc5e9d146656bf58cfe6a69aa5a540bf
Katie Wilson <kmcwilson4@gmail.com>
2022-11-08 15:00:47 -0500

    adding to the functions for each choice by the user

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const sql = require('./db/scheme.sql');
-const { isAsyncFunction } = require('util/types');
-const { allowedNodeEnvironmentFlags } = require('process');
+const consoleTable = require('console.table');
+const mysql = require('mysql2');
 
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        port: '3306',
+        user: 'root',
+        password: '',
+        database: 'employee_db'
+    },
+    console.log('Connected to the employee_db.')
+)
 
 console.table(
     "\n------------ WELCOME TO THE EMPLOYEE TRACKER ------------\n"
-)
+);
 
 
 const firstQuestions = async () => {
@@ -41,16 +50,69 @@ const firstQuestions = async () => {
                 updateEmployee();
                 break;
 
-
-
-
         }
     }
     catch (err) { console.log(err) };
     firstQuestions();
 };
 
-const addDepartment= () => {
-    
+const addDepartment = async () => {
+    try {
+        console.log('Adding to departments');
+        await inquirer.prompt([
+            {
+                name: 'department',
+                type: 'input',
+                message: 'What is the name of the new department?'
+            }
+        ]);
+        await db.query('INSERT INTO departments SET ?', {
+            departments_names: department.message
+        });
+        console.log(`${department.message} added to the departments table!`)
+        firstQuestions();
+    }
+    catch (err) {
+            console.log(err);
+        }
+    };
 
-}
+
+
+const addRole =
+    db.query('INSERT INTO roles (title)', (err, result) => {
+        if (err) {
+            console.log(err);
+        }
+        console.log(`${result} added to roles!`);
+    });
+
+const addEmployee =
+    db.query('INSERT INTO employees (title, first_name, last_name)', (err, result) => {
+        if (err) {
+            console.log(err);
+        }
+        console.log(`${result} added to employee list!`);
+    });
+
+const viewRoles =
+    db.query('SELECT * FROM roles', (err, result) => {
+        console.log(result);
+    });
+
+const viewDepartments =
+    db.query('SELECT * FROM departments', (err, result) => {
+        console.log(result);
+    });
+
+const viewEmployees =
+    db.query('SELECT * FROM employees', (err, result) => {
+        console.log(result);
+    });
+
+const updateEmployee =
+    db.query(`UPDATE employees SET name = ${inserted.name}`, (err, result) => {
+        console.log(result);
+    })
+
+
