3e36dca8a125588178b685970f3623220ff136a7
Katie Wilson <kmcwilson4@gmail.com>
2022-11-22 11:33:15 -0500

    Adjusted the connection.js

--- db/connections.js
+++ db/connections.js
@@ -4,8 +4,8 @@ const mysql = require('mysql2');
 const db = mysql.createConnection(
     {
         host: 'localhost',
-        user: 'root',
-        password: 'Marley1989#',
+        user: '',
+        password: '',
         database: 'employee_db'
     },
     console.log('Connected to the employee_db.')
